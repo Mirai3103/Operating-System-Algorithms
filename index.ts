757c4d4b1135c8873a017c3f71423faa2b4a2e08
Mirai3103 <huuhoag1412@gmail.com>
2023-11-06 10:27:34 +0700

    them moc thoi gian vao

--- index.ts
+++ index.ts
@@ -2,24 +2,16 @@ import SchedulingService, { ProcessTaskQueue, SchedulerType, Task, TaskType } fr
 import createSolution from "./CPUSScheduling/index";
 
 const process1 = new ProcessTaskQueue("P1", 0);
-process1.addTask(new Task(TaskType.CPU, 8));
-process1.addTask(new Task(TaskType.IO1, 5));
-process1.addTask(new Task(TaskType.CPU, 1));
+process1.addTask(new Task(TaskType.CPU, 15));
 
-const process2 = new ProcessTaskQueue("P2", 2);
-process2.addTask(new Task(TaskType.CPU, 1));
-process2.addTask(new Task(TaskType.IO2, 8));
-process2.addTask(new Task(TaskType.CPU, 2));
-process2.addTask(new Task(TaskType.IO1, 5));
-const process3 = new ProcessTaskQueue("P3", 10);
-process3.addTask(new Task(TaskType.CPU, 6));
-process3.addTask(new Task(TaskType.IO1, 5));
-process3.addTask(new Task(TaskType.CPU, 2));
-process3.addTask(new Task(TaskType.IO2, 3));
+const process2 = new ProcessTaskQueue("P2", 1);
+process2.addTask(new Task(TaskType.CPU, 20));
 
-const process4 = new ProcessTaskQueue("P4", 11);
-process4.addTask(new Task(TaskType.CPU, 3));
-process4.addTask(new Task(TaskType.IO2, 20));
+const process3 = new ProcessTaskQueue("P3", 5);
+process3.addTask(new Task(TaskType.CPU, 10));
 
-const solution = createSolution(SchedulerType.RR, [process1, process2, process3, process4], 4);
+const process4 = new ProcessTaskQueue("P4", 7);
+process4.addTask(new Task(TaskType.CPU, 22));
+
+const solution = createSolution(SchedulerType.RR, [process1, process2, process3, process4], 3);
 solution.run();
